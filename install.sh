13ab3b727fd11bff4705f1645d10cf4c0432012a
nattapan <3727102+nattapan@users.noreply.github.com>
2020-09-17 17:31:34 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,4 +19,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
    
    sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
    sudo chmod +x /usr/local/bin/docker-compose
-   
+   sudo docker pull portainer/portainer
+   sudo docker volume create portainer_data 
+   sudo docker run -d -p 8000:8000 -p 80:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data --restart  always portainer/portainer
+   sudo docker ps
