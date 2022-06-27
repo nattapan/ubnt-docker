8899c31da2fcd970ad155ecd847c095bde933bd9
nattapan <3727102+nattapan@users.noreply.github.com>
2022-06-27 16:51:10 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,5 +22,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
    sudo chmod +rx /usr/local/bin/docker-compose
    sudo docker pull portainer/portainer-ce:latest
    sudo docker volume create portainer_data 
-   sudo docker run -d -p 8000:8000 -p 8080:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data --restart  always portainer/portainer-ce
+   sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+   #sudo docker run -d -p 8000:8000 -p 8080:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data --restart  always portainer/portainer-ce
    sudo docker ps
