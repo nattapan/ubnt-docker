00ae6d5d294b974bb17d813adbde8ce4902388ed
nattapan <3727102+nattapan@users.noreply.github.com>
2025-11-10 16:54:22 +0700

    Update get-geoip-dat.sh

--- get-geoip-dat.sh
+++ get-geoip-dat.sh
@@ -1,3 +1,3 @@
-wget -q -N -P /var/lib/Geoip https://dl.miyuru.lk/geoip/dbip/country/dbip4.dat.gz
-cd /var/lib/Geoip
+wget -q -N -P /var/lib/GeoIP https://dl.miyuru.lk/geoip/dbip/country/dbip4.dat.gz
+cd /var/lib/GeoIP
 gzip -dkf dbip4.dat.gz 
